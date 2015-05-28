5bc783e90898564a8b4304a9a17cf4255b696565
Ross Pace <rpace@avantia-inc.com>
2015-05-28 16:49:46 -0400

    removing table based forms layout - the implementation seems too buggy and doesn't display well on phones

--- XScaffolding/Views.cs
+++ XScaffolding/Views.cs
@@ -14,52 +14,52 @@ namespace MYPROJECT.Views
 		{
 			this.Title = MYPROJECT.Resources.MyModelTitle;
 
-			var entryField = new EntryCell { Label = MYPROJECT.Resources.EntryLabel };
+			var entryField = new Entry {  };
 			entryField.SetBinding(EntryCell.TextProperty, new Binding("Entry", BindingMode.TwoWay, source: Model));
 
 			var multiLineEntryField = new Editor();
-			var multiLineEntryFieldView = new ViewCell { View = new StackLayout { Children = { new Label { Text = MYPROJECT.Resources.MultiLineEntryLabel }, multiLineEntryField } } };
 			multiLineEntryField.SetBinding(Editor.TextProperty, new Binding("MultiLineEntry", BindingMode.TwoWay, source: Model));
 
-			var textField = new TextCell { Text = MYPROJECT.Resources.TextLabel, Detail = Model.Text };
+			var textField = new Label { Text = Model.Text };
 
-			var switchField = new SwitchCell { Text = MYPROJECT.Resources.SwitchLabel };
-			switchField.SetBinding(SwitchCell.OnProperty, new Binding("Switch", BindingMode.TwoWay, source: Model));
+			var switchField = new Switch();
+			switchField.SetBinding(Switch.IsToggledProperty, new Binding("Switch", BindingMode.TwoWay, source: Model));
 
-			var pickerField = new Picker { Title = MYPROJECT.Resources.PickerLabel };
+			var pickerField = new Picker();
 			pickerField.Items.Add(MYPROJECT.Resources.FredOption);
 			pickerField.Items.Add(MYPROJECT.Resources.WilmaOption);
 			pickerField.Items.Add(MYPROJECT.Resources.BarneyOption);
 			pickerField.Items.Add(MYPROJECT.Resources.BettyOption);
 			pickerField.SelectedIndex = pickerField.Items.IndexOf(Model.Picker);
 			pickerField.SelectedIndexChanged += (object sender, EventArgs e) => { Model.Picker = pickerField.SelectedIndex > -1 ? pickerField.Items[pickerField.SelectedIndex]: null; };
-			var pickerFieldView = new ViewCell { View = new StackLayout { Children = { new Label { Text = MYPROJECT.Resources.PickerLabel }, pickerField } } };
 
 			var sliderField = new Slider { Minimum = 1, Maximum = 10 };
 			sliderField.SetBinding(Slider.ValueProperty, new Binding("Slider", BindingMode.TwoWay, source: Model));
-			var sliderFieldView = new ViewCell { View = new StackLayout { Children = { new Label { Text = MYPROJECT.Resources.SliderLabel }, sliderField } } };
 
 			var stepperField = new Stepper { Increment = 1, Minimum = 1, Maximum = 5 };
 			stepperField.SetBinding(Stepper.ValueProperty, new Binding("Stepper", BindingMode.TwoWay, source: Model));
-			var stepperFieldView = new ViewCell { View = new StackLayout { Children = { new Label { Text = MYPROJECT.Resources.StepperLabel }, stepperField } } };
 
 			var dateField = new DatePicker { Format = "D" };
 			dateField.SetBinding(DatePicker.DateProperty, new Binding("Date", BindingMode.TwoWay, source: Model));
-			var dateFieldView = new ViewCell { View = new StackLayout { Children = { new Label { Text = MYPROJECT.Resources.DateLabel }, dateField } } };
 
 			var timeField = new TimePicker { Format = "T" };
- 			timeField.SetBinding(TimePicker.TimeProperty, new Binding("Time", BindingMode.TwoWay, source: Model));
-			var timeFieldView = new ViewCell { View = new StackLayout { Children = { new Label { Text = MYPROJECT.Resources.TimeLabel }, timeField } } };
+			timeField.SetBinding(TimePicker.TimeProperty, new Binding("Time", BindingMode.TwoWay, source: Model));
 
-			this.Content = new TableView
+			this.Content = new StackLayout
 			{
-				Intent = TableIntent.Form,
-				Root = new TableRoot
+				Orientation = StackOrientation.Vertical,
+				Spacing = 15,
+				Children = 
 				{
-					new TableSection
-					{
-						entryField, multiLineEntryFieldView, textField, switchField, pickerFieldView, sliderFieldView, stepperFieldView
-					}
+					new StackLayout { Orientation = StackOrientation.Vertical, Children = { new Label { Text = MYPROJECT.Resources.EntryLabel, FontAttributes = FontAttributes.Bold }, entryField } },
+					new StackLayout { Orientation = StackOrientation.Vertical, Children = { new Label { Text = MYPROJECT.Resources.MultiLineEntryLabel, FontAttributes = FontAttributes.Bold }, multiLineEntryField } },
+					new StackLayout { Orientation = StackOrientation.Vertical, Children = { new Label { Text = MYPROJECT.Resources.TextLabel, FontAttributes = FontAttributes.Bold }, textField } },
+					new StackLayout { Orientation = StackOrientation.Vertical, Children = { new Label { Text = MYPROJECT.Resources.SwitchLabel, FontAttributes = FontAttributes.Bold }, switchField } },
+					new StackLayout { Orientation = StackOrientation.Vertical, Children = { new Label { Text = MYPROJECT.Resources.PickerLabel, FontAttributes = FontAttributes.Bold }, pickerField } },
+					new StackLayout { Orientation = StackOrientation.Vertical, Children = { new Label { Text = MYPROJECT.Resources.SliderLabel, FontAttributes = FontAttributes.Bold }, sliderField } },
+					new StackLayout { Orientation = StackOrientation.Vertical, Children = { new Label { Text = MYPROJECT.Resources.StepperLabel, FontAttributes = FontAttributes.Bold }, stepperField } },
+					new StackLayout { Orientation = StackOrientation.Vertical, Children = { new Label { Text = MYPROJECT.Resources.DateLabel, FontAttributes = FontAttributes.Bold }, dateField } },
+					new StackLayout { Orientation = StackOrientation.Vertical, Children = { new Label { Text = MYPROJECT.Resources.TimeLabel, FontAttributes = FontAttributes.Bold }, timeField } }
 				}
 			};
 		}
